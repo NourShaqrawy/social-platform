9877b87b0a3865cdcd86cd7b7f41086eee24d980
NourShaqrawy <shkrawysaleh037@gmail.com>
2026-04-11 10:12:29 +0300

    hh

--- entrypoint.sh
+++ entrypoint.sh
@@ -10,12 +10,12 @@ php artisan config:clear
 php artisan view:clear
 
 
-# php artisan migrate:fresh --force
+php artisan migrate:fresh --force
 
-# php artisan migrate
-# # احذر من migrate:fresh لأنه يحذف الجداول ويعيد إنشائها، استخدمه فقط إذا تريد إعادة قاعدة البيانات كاملة
+php artisan migrate
+# احذر من migrate:fresh لأنه يحذف الجداول ويعيد إنشائها، استخدمه فقط إذا تريد إعادة قاعدة البيانات كاملة
 
-# php artisan db:seed
+php artisan db:seed
 
 # تشغيل Apache في المقدمة
 exec apache2-foreground
